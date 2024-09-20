9ec83cf61b82744c4b1080a305c50aaa51905eea
AnnaGlazkova17 <glazkova1706@gmail.com>
2024-09-20 22:13:52 +0300

    module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -3,5 +3,5 @@ immutable_var = 1,'Привет',3.14,'Hello',True # кортеж содержи
 # immutable_var [0] = 101 - нельзя изменить элемент кортежа, т.к. кортеж относится к неизменяемым типам
 print('Immutable tuple:', immutable_var) # вывод кортежа на экран
 mutable_list= ['Я',1,'делаю шаг, делаю',2,False] # создала список
-mutable_list[-1]= True
-print('Mutable list:',mutable_list)
+mutable_list[-1]= True # заменила элемент списка
+print('Mutable list:',mutable_list) # вывела список на экран
